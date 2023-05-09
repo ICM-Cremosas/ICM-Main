0da74458a2c9cca9a7a0ae38238f3e7499d99d0d
sauce <telmobelasauce@ua.pt>
2023-05-09 12:11:14 +0100

    seeksbars

--- AREC/app/src/main/java/com/example/arec/CreateEvent.kt
+++ AREC/app/src/main/java/com/example/arec/CreateEvent.kt
@@ -2,6 +2,7 @@ package com.example.arec
 
 import android.os.Bundle
 import android.view.*
+import android.widget.SeekBar
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -16,6 +17,9 @@ class CreateEvent : Fragment() {
 
     var database: FirebaseDatabase? = null
     lateinit var binding : CreateEventBinding
+    var radius : Int = 2000
+    var duration : Int = 2
+    var participants : Int = 20
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate<CreateEventBinding>(inflater, R.layout.create_event,container,false)
 
@@ -30,25 +34,65 @@ class CreateEvent : Fragment() {
 
         val latlng = arguments?.getParcelable<LatLng>("LatLngUser")
         if (latlng != null){
+            //Radius
+            binding.eventRadius.progress = radius
+            binding.radius.text = "Radius: $radius Km"
+
+            binding.eventRadius.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                    // Update the TextView with the current progress value
+                    binding.radius.text = "Radius: $progress Km"
+                    radius = progress
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                    // Do nothing
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                    // Do nothing
+                }
+            })
+
+            //duration
+            binding.eventDuration.progress = duration
+            binding.duration.text = "Duration: $duration H"
+
+            binding.eventDuration.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                    // Update the TextView with the current progress value
+                    binding.duration.text = "Duration: $progress H"
+                    duration = progress
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+            })
+
+
+            //participants
+            binding.eventParticipants.progress = participants
+            binding.participants.text = "Participants: $participants"
+
+            binding.eventParticipants.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                    // Update the TextView with the current progress value
+                    binding.participants.text = "Participants: $progress"
+                    participants = progress
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+            })
             binding.butCreateEvent.setOnClickListener {
                 if(binding.eventName.text.toString().isEmpty()){
                     binding.eventName.setError("Please type a name")
                 }
-                if(binding.eventRadius.text.toString().isEmpty()){
-                    binding.eventRadius.setError("Please type a radius")
-                }
-                if(binding.eventDuration.text.toString().isEmpty()){
-                    binding.eventDuration.setError("Please type a duration")
-                }
-                if(binding.eventParticipants.text.toString().isEmpty()){
-                    binding.eventParticipants.setError("Please type a totalParticipants")
-                }
-                if(!(binding.eventName.text.toString().isEmpty() || binding.eventRadius.text.toString().isEmpty()
-                    || binding.eventDuration.text.toString().isEmpty() || binding.eventParticipants.text.toString().isEmpty())) {
+                if(!(binding.eventName.text.toString().isEmpty())) {
                     val eventName = binding.eventName.text.toString()
-                    val radius = binding.eventRadius.text.toString().toDouble()
-                    val duration = binding.eventDuration.text.toString().toDouble()
-                    val totalParticipants = binding.eventParticipants.text.toString().toInt()
+                    val radius = radius.toDouble()
+                    val duration = duration.toDouble()
+                    val totalParticipants = participants.toInt()
                     val randomKey = database!!.reference.push().key
                     val event = Event(eventName, randomKey!!, FirebaseAuth.getInstance().uid!!, radius, latlng, duration, totalParticipants)
                     database!!.reference
